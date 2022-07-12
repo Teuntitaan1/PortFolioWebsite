e91152e75724ed178bb372af65b488a9d993c3de
Teuntitaan1 <Teun.weijdener@gmail.com>
2022-07-12 09:59:00 +0200

    added different pages

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ $(document).ready(function(){
   $("body").show(1000);
   console.log("The page loaded!");
 })
+function changepage(towhat) {
+  $("body").show(1000);
+  window.location.href = towhat;
+}
