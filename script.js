11e50edf45397e59399ef903180e0a2d13fe991f
Teuntitaan1 <Teun.weijdener@gmail.com>
2022-07-12 21:26:45 +0200

    Added the About Me page

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
-$("body").hide()
+//$("body").hide()
 
 $(document).ready(function(){
-  $("body").show(1000);
+  //$("body").show(1000);
   console.log("The page loaded!");
+  $("#yearsssincebirth").text(dayssincebirth(new Date(), new Date('2/14/2007'), 3));
+  $("#yearssinceprogramming").text(dayssincebirth(new Date(), new Date('3/15/2020'), 1));
 })
+
 function changepage(towhat) {
   $("body").show(1000);
   window.location.href = towhat;
+}
+
+function dayssincebirth(firstdate, secondate, roundamount) {
+    let difference = firstdate.getTime() - secondate.getTime();
+    // converts it from seconds to years
+    let TotalDays = (difference / (1000 * 3600 * 24 * 365)).toFixed(roundamount);
+    
+    return TotalDays;
 }
